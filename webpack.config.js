719fcd3ea387221c22f909da144f5555ce685fb6
Your Name <anoor@witweel.com>
2017-10-03 20:53:52 -0400

    fix indent

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
+
+const debug = process.env.NODE_ENV !== "production";
 const slsw = require("serverless-webpack");
 const nodeExternals = require("webpack-node-externals");
 
 const slsw = require("serverless-webpack");
 const nodeExternals = require("webpack-node-externals");
 module.exports = {
-		 entry: '..handler.js',
+		 entry: 'slsw.lib.entries',
 		 target: "node",
 		 // Since 'aws-sdk' is not compatible with webpack,
 		 // we exclude all node dependencies
  externals: [nodeExternals()],
  // Run babel on all .js files and skip those in node_modules
  module: {
- rules: [
+loader: [
   {
 	 test: /\.js$/,
 	 loader: "babel-loader",
@@ -20,7 +22,19 @@ module.exports = {
   }
     ]
   }
-}
+},
+output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: './handler.js'
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  
+  ],
+};
 
 
 //ref:https://github.com/serverless-heaven/serverless-webpack
